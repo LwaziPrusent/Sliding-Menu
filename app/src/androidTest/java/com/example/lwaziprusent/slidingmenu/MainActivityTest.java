6808d6eab224f43942413897ffeebae7851a780b
LwaziPrusent <lwazi.prusent@gmail.com>
2015-07-25 17:43:31 +0200

    added tests

---
+++ app/src/androidTest/java/com/example/lwaziprusent/slidingmenu/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.lwaziprusent.slidingmenu;
+
+import android.util.Log;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lwazi.prusent on 2015-07-25.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testOnCreate() throws Exception {
+        Log.i("lwazi test","Lwazi Test");
+        System.out.println("lwazi test");
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+}
---
+++ app/src/test/java/com/example/lwaziprusent/slidingmenu/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.lwaziprusent.slidingmenu;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lwazi.prusent on 2015-07-25.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+}
